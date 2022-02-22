8ff3d00fe33dde110f09e63d1496dc786ce63345
arturo.opsetmoen.amador <AMAART@banenor.no>
2022-02-22 14:02:48 +0100

    Move deps to setup.py

--- setup.py
+++ setup.py
@@ -5,4 +5,17 @@ setuptools.setup(
     version="0.0.0",
     description="Starter code.",
     author="Student",
+    install_requires=[
+        "pandas==1.4.0",
+        "numpy==1.22.2",
+        "scikit-learn==1.0.2",
+        "dvc[s3]==2.9.3",
+        "pytest==6.2.5",
+        "requests==2.27.1",
+        "fastapi==0.73.0",
+        "uvicorn==0.17.4",
+        "gunicorn==20.1.0",
+        "scikit-optimize[plots]==0.9.0",
+        "xgboost==1.5.2"
+        ]
 )
