7368ab03a6d43310519e596b0f09b71ebb21bfb3
arturo.opsetmoen.amador <AMAART@banenor.no>
2022-02-24 16:02:06 +0100

    fix api tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -28,7 +28,7 @@ def test_api_get_root():
 
 def test_api_get_message():
     """
-    Tests the GET method for the implemented API.
+    Tests the GET Welcome message for the implemented API.
     Returns: None
     -------
 
@@ -47,22 +47,81 @@ def test_api_get_message():
             raise err
 
 
-def test_positive(positive_ex):
+def test_positive(positive_ex) -> None:
+    """
+    Tests the post/predict API method for our web app. Example taken from the dataset to ensure a positive
+    prediction.
+    Parameters
+    ----------
+    positive_ex: Fixture positive_ex as defined in conftest.py with session scope.
+
+    Returns: None
+    -------
+
+    """
     with TestClient(app) as client:
-        r = client.post("/predict", data=json.dumps(positive_ex),
-                        headers={"Content-Type": "application/json"})
-        assert r.json()["Salary"] == "1", "Unexpected output of the model"
+        try:
+            r = client.post("/predict", data=json.dumps(positive_ex), headers={"Content-Type": "application/json"})
+            assert r.json()["Salary"] == "1"
+            logging.info(
+                "Testing POST/predict method: The prediction is correct: SUCCESS"
+            )
+        except AssertionError as err:
+            logging.error(
+                "Testing POST/predict method: The prediction is not correct: ERROR"
+            )
+            raise err
 
 
-def test_negative(negative_ex):
+
+
+def test_negative(negative_ex) -> None:
+    """
+    Tests the post/predict API method for our web app. Example taken from the dataset to ensure a negative
+    prediction.
+    Parameters
+    ----------
+    negative_ex: Fixture positive_ex as defined in conftest.py with session scope.
+
+    Returns: None
+    -------
+
+    """
     with TestClient(app) as client:
-        r = client.post("/predict", data=json.dumps(negative_ex),
-                        headers={"Content-Type": "application/json"})
-        assert r.json()["Salary"] == "0", "Unexpected output of the model"
+        try:
+            r = client.post("/predict", data=json.dumps(negative_ex), headers={"Content-Type": "application/json"})
+            assert r.json()["Salary"] == "0"
+            logging.info(
+                "Testing POST/predict method: The prediction is correct: SUCCESS"
+            )
+        except AssertionError as err:
+            logging.error(
+                "Testing POST/predict method: The prediction is not correct: ERROR"
+            )
+            raise err
 
 
-def test_response(positive_ex):
+def test_post_code(positive_ex):
+    """
+    Tests the post/predict API method for our web app. This tests checks the response code (200) from the API.
+    Parameters
+    ----------
+    positive_ex: Fixture positive_ex as defined in conftest.py with session scope. Tests can also run with neg. fixture.
+
+    Returns: None
+    -------
+
+    """
     with TestClient(app) as client:
-        r = client.post("/predict", data=json.dumps(positive_ex),
-                        headers={"Content-Type": "application/json"})
-        assert r.status_code == 200
+        try:
+            r = client.post("/predict", data=json.dumps(positive_ex), headers={"Content-Type": "application/json"})
+            assert r.status_code == 200
+            logging.info(
+                "Testing POST/predict returned code: The code is correct: SUCCESS"
+            )
+        except AssertionError as err:
+            logging.error(
+                "Testing POST/predict returned code: The code is not correct: SUCCESS"
+            )
+            raise err
+
