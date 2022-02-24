da69f230d2240933c7e4c5b1a2bca09b9f582179
arturo.opsetmoen.amador <AMAART@banenor.no>
2022-02-24 13:13:13 +0100

    Refactor clearning routine

--- eda/data_cleaning.py
+++ eda/data_cleaning.py
@@ -1,20 +1,25 @@
 """
-Cleaning method
+Cleaning method. Implements a very basic cleaning routine. Features with low data
+quality, such as capital-gain and -loss are dropped.
 """
 import pandas as pd
+import numpy as np
 
 
-def clean_data(df):
-    df.replace({'?': None}, inplace=True)
-    df.dropna(inplace=True)
-    df.drop("capital-gain", axis="columns", inplace=True)
-    df.drop("capital-loss", axis="columns", inplace=True)
-    df.drop("education-num", axis="columns", inplace=True)
-    df.drop("fnlgt", axis="columns", inplace=True)
-    return df
+def clean_data(data_frame: pd.DataFrame) -> pd.DataFrame:
+    # Replace string ?
+    data_frame.replace({'?': np.nan}, inplace=True)
+    data_frame.dropna(inplace=True)
+    # Drop features with low data quality. This schame needs to be implemented also during
+    # predictions.
+    data_frame.drop("capital-gain", axis="columns", inplace=True)
+    data_frame.drop("capital-loss", axis="columns", inplace=True)
+    data_frame.drop("education-num", axis="columns", inplace=True)
+    data_frame.drop("fnlgt", axis="columns", inplace=True)
+    return data_frame
 
 
 if __name__ == "__main__":
-    df = pd.read_csv("../data/census.csv", skipinitialspace=True)
-    clean_df = clean_data(df)
-    df.to_csv("../data/census_clean_v1.csv", index=False)
+    data_frame = pd.read_csv("../data/census.csv", skipinitialspace=True)
+    clean_df = clean_data(data_frame)
+    clean_df.to_csv("../data/census_clean_v1.csv", index=False)
