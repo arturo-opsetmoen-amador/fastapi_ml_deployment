a6bde2f2e06aae37efc3c4f3152504040f8ae831
arturo.opsetmoen.amador <AMAART@banenor.no>
2022-02-24 15:47:13 +0100

    Improve API tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,13 +2,49 @@ import pytest
 from fastapi.testclient import TestClient
 from main import app
 import json
+import logging
 
 
-def test_api_locally_get_root():
+def test_api_get_root():
+    """
+    Tests the GET method for the implemented API.
+    Returns: None
+    -------
+
+    """
     with TestClient(app) as client:
-        r = client.get('/')
-        assert r.status_code == 200, "Status code is not 200"
-        assert r.json()["Welcome"] == "Welcome to the salary XGBoost predictor web service.", "Wrong json output"
+        try:
+            r = client.get('/')
+            assert r.status_code == 200
+            logging.info(
+                "Testing GET method API: Status code 200 delivered: SUCCESS"
+            )
+        except AssertionError as err:
+            logging.error(
+                "Testing GET method API: Status code 200 not delivered: ERROR"
+            )
+            raise err
+
+
+def test_api_get_message():
+    """
+    Tests the GET method for the implemented API.
+    Returns: None
+    -------
+
+    """
+    with TestClient(app) as client:
+        try:
+            r = client.get('/')
+            assert r.json()["Welcome"] == "Welcome to the salary XGBoost predictor web service."
+            logging.info(
+                "Testing GET method Welcome message: The message delivered is correct: SUCCESS"
+            )
+        except AssertionError as err:
+            logging.error(
+                "Testing GET method Welcome message: The message delivered is not correct: ERROR"
+            )
+            raise err
 
 
 def test_positive(positive_ex):
